0a327459916731b1234e97ba8e362548f90feca8
HFsa-RaShid <hafsa20cse028@gmail.com>
2026-03-08 17:04:27 +0600

    update link

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  baseURL: "http://beautyzone-nextjs-server.vercel.app",
+  baseURL: "https://beautyzone-nextjs-server.vercel.app",
   withCredentials: true,
 });
 const useAxiosPublic = () => {
